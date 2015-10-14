47332bbfc7aaa4f7565be51bf87b54c1f427d23f
Cătălin Mariș <alrraa@gmail.com>
2015-10-14 22:11:15 +0300

    Specify a valid SPDX license in `package.json`
    Update the `license` field in the `package.json` file so that its value
    is a valid SPDX license expression.
    
     " npm WARN EPACKAGEJSON apache-server-configs@2.14.0
       license should be a valid SPDX license expression "
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Ref: https://docs.npmjs.com/files/package.json#license

--- bin/build/create_header.js
+++ bin/build/create_header.js
@@ -1,6 +1,6 @@
 var pkg = require('./../../package.json');
 
-var string = '# Apache Server Configs v' + pkg.version + ' | ' + pkg.license.type +' License\n' +
+var string = '# Apache Server Configs v' + pkg.version + ' | ' + pkg.license + ' License\n' +
              '# ' + pkg.homepage + '\n\n';
 
 process.stdout.write(string);
