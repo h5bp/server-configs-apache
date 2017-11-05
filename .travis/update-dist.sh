d80cc571c7d62e5726dce47dd4bbe9cce70c5576
Cătălin Mariș <alrraa@gmail.com>
2017-11-05 06:49:27 -0800

    Make Travis CI auto update content from `dist/`
    Fix #118

---
+++ .travis/update-dist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Automatically update the content from the `dist/` directory.
+# (See: https://github.com/alrra/travis-scripts#readme)
+
+$(npm bin)/set-up-ssh --key "$encrypted_22ef8dc7aed9_key" \
+                      --iv "$encrypted_22ef8dc7aed9_iv" \
+                      --path-encrypted-key "github-deploy-key.enc" \
+    && $(npm bin)/commit-changes --branch "master" \
+                                 --commands "npm run build" \
+                                 --commit-message "Update the generated content [skip ci]"
+
---
+++ bin/setup/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install Apache
+
+sudo apt-get update
+sudo apt-get install apache2 #=${APACHE_VERSION}.\*
+
+# Enable required modules
+
+sudo a2enmod autoindex deflate expires filter headers include mime rewrite setenvif
+
+# Configure apache virtual hosts
+
+sudo cp -f bin/setup/${APACHE_VERSION}.conf /etc/apache2/sites-available/${CONF_TARGET}
+sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)/test/fixtures?g" --in-place /etc/apache2/sites-available/${CONF_TARGET}
+sudo chmod 777 -R $HOME
+
+# Add `ServerTokens Prod` to the main configuration file in order
+# to test if Apache hides the server software information (this
+# directive doesn't work in the `.htaccess` file).
+#
+# https://httpd.apache.org/docs/current/mod/core.html#servertokens
+
+sudo sh -c "printf 'ServerTokens Prod\n' >> /etc/apache2/apache2.conf"
+
+# Check config and restart apache
+
+sudo service apache2 restart
